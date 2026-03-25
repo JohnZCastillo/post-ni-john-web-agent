75fe5cb27a78666e606a9b57b5780d3809ee2ef4
john <johnzunigacastillo@gmail.com>
2026-03-25 09:53:39 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { cloneRawRequest } from 'hono/request'
+
+const app = new Hono()
+
+app.all('/agent', async (c) => {
+
+  const targetUrl = c.req.query('targetUrl');
+
+  const clonedReq = await cloneRawRequest(c.req)
+  
+  return fetch(targetUrl!, clonedReq)
+
+})
+
+app.onError((error,c) => {
+  console.error(`${error.message}`)
+  return c.text('Custom Error Message', 500)
+})
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+})
